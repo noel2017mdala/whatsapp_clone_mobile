f073a8c4f1d21aac12c03cc254978869fd1a7b3b
noel2017mdala <noelmdala2017@gmail.com>
2022-02-15 12:02:51 +0200

    adding group selector and image upload

--- src/components/CreateGroup.js
+++ src/components/CreateGroup.js
@@ -10,9 +10,12 @@ import {
   RefreshControl,
   Image,
   ImageBackground,
-  CheckBox,
+  Platform,
+  Button,
 } from "react-native";
-//4461
+import * as ImagePicker from "expo-image-picker";
+import Constant from "expo-constants";
+import Checkbox from "expo-checkbox";
 import env from "../utils/env";
 import tw from "tailwind-react-native-classnames";
 import Icon from "react-native-vector-icons/AntDesign";
@@ -21,6 +24,8 @@ import { HeaderTab } from "../utils/NavigationHeader";
 const CreateGroup = () => {
   let [checkedUsers, setCheckedUsers] = useState([]);
   const [refreshing, SetRefresh] = useState(false);
+  const [createGroup, setCreateGroup] = useState(false);
+  const [userImage, setUserImage] = useState(null);
   const dispatch = useDispatch();
   const select = useSelector((e) => {
     return e;
@@ -30,142 +35,222 @@ const CreateGroup = () => {
     dispatch(FetchUserContactList(select.UserData, select.Tokens));
   }, []);
 
-  const handleCheck = (params) => {
-    console.log(params);
+  const checkPermission = async () => {
+    if (Platform.OS !== "web") {
+      const { status } =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status === "granted") {
+        selectImage();
+      } else {
+        return false;
+      }
+    }
+  };
+
+  const selectImage = async () => {
+    let res = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(res);
+
+    if (!res.cancelled) {
+      setUserImage(res.uri);
+    }
   };
   return (
     <>
-      <HeaderTab name="Create Group" />
-      <View
-        style={[
-          tw`relative `,
-          {
-            flex: 1,
-          },
-        ]}
-      >
-        {/* <Text>Hello Create Group</Text> */}
-        {!select.ContactList ? null : !select.ContactList.data ? null : (
-          <FlatList
-            keyExtractor={(item) => item._id}
-            data={select.ContactList.data}
-            refreshControl={
-              <RefreshControl
-                refreshing={refreshing}
-                colors={["#128C7E"]}
-                onRefresh={() => {
-                  console.log("user is about to refresh");
-                  return;
-                  SetRefresh(true);
-                  dispatch(
-                    FetchUserContactList(select.UserData, select.Tokens)
-                  );
-                  setTimeout(() => {
-                    SetRefresh(false);
-                  }, 4000);
-                }}
-              />
-            }
-            renderItem={({ item }) => (
-              <View style={[tw`ml-2 mt-2`]}>
-                <TouchableOpacity>
-                  <View
-                    style={[
-                      tw`h-16 mb-2  mt-1 border-b-2 border-gray-200 flex flex-row`,
-                      {
-                        width: "100%",
-                      },
-                    ]}
-                  >
-                    <TouchableOpacity>
-                      <View
-                        style={[
-                          tw``,
-                          {
-                            paddingLeft: "4%",
-                          },
-                        ]}
-                      >
-                        <Image
-                          source={{
-                            uri: `${env.DEV_SERVER_URL}api/v1/users/getImage/${item.profileImage}`,
-                          }}
-                          style={[tw` rounded-full w-14 h-14`]}
-                        />
-                      </View>
-                    </TouchableOpacity>
-
-                    <View
-                      style={[
-                        tw`flex `,
-                        {
-                          marginLeft: "2%",
-                        },
-                      ]}
-                    >
-                      <View style={[tw``, {}]}>
-                        <Text style={[tw`font-bold text-lg`, {}]}>
-                          {item.name}
-                        </Text>
-                      </View>
-
-                      <View>
-                        <View>
-                          <Text
-                            style={[
-                              tw`overflow-hidden italic`,
-                              {
-                                width: 210,
-                              },
-                            ]}
-                            numberOfLines={1}
-                            ellipsizeMode="tail"
-                          >
-                            {item.userAbout}
-                          </Text>
-                        </View>
-                      </View>
-                    </View>
-                    <View style={[tw``, {}]}>
-                      <CheckBox
-                        name={item}
-                        checked={true}
-                        onPress={handleCheck}
-                        key={item._id}
-                      />
-                    </View>
-                  </View>
-                </TouchableOpacity>
-              </View>
-            )}
+      {createGroup ? (
+        <>
+          <HeaderTab
+            name="Create Group"
+            navigateBack={() => {
+              setCreateGroup(false);
+            }}
           />
-
-          // <Text>Hello</Text>
-        )}
-
-        <View
-          style={[
-            tw`absolute bottom-0 mb-8 flex items-center justify-center`,
-            {
-              width: "100%",
-            },
-          ]}
-        >
-          {/* <View style={[tw`bg-green-400 w-14 h-14 rounded-full`]}>
-            <Text>Hello World</Text>
-          </View> */}
-          <TouchableOpacity
+          <View
             style={[
-              tw`bg-green-400 w-14 h-14 rounded-full flex items-center justify-center`,
+              tw`items-center justify-center`,
               {
-                backgroundColor: "#128C7E",
+                flex: 1,
               },
             ]}
           >
-            <Icon name="arrowright" size={25} color="#FFFFFF" />
-          </TouchableOpacity>
-        </View>
-      </View>
+            {/* <Text>Hello World</Text> */}
+            <Button title="Select Image" onPress={checkPermission} />
+            {userImage && (
+              <Image
+                source={{ url: userImage }}
+                style={{
+                  width: 200,
+                  height: 200,
+                }}
+              />
+            )}
+          </View>
+        </>
+      ) : (
+        <>
+          <HeaderTab name="Add Participants" />
+          <View
+            style={[
+              tw`relative `,
+              {
+                flex: 1,
+              },
+            ]}
+          >
+            {/* <Text>Hello Create Group</Text> */}
+            {!select.ContactList ? null : !select.ContactList.data ? null : (
+              <FlatList
+                keyExtractor={(item) => item._id}
+                data={select.ContactList.data}
+                refreshControl={
+                  <RefreshControl
+                    refreshing={refreshing}
+                    colors={["#128C7E"]}
+                    // onRefresh={() => {
+                    //   SetRefresh(true);
+                    //   dispatch(
+                    //     FetchUserContactList(select.UserData, select.Tokens)
+                    //   );
+                    //   setTimeout(() => {
+                    //     SetRefresh(false);
+                    //   }, 4000);
+                    // }}
+                  />
+                }
+                renderItem={({ item }) => (
+                  <View style={[tw`ml-2 mt-2`]}>
+                    <TouchableOpacity
+                      activeOpacity={0.7}
+                      onPress={() => {
+                        if (!checkedUsers.includes(item._id)) {
+                          setCheckedUsers([...checkedUsers, item._id]);
+                        } else {
+                          setCheckedUsers([
+                            ...checkedUsers.filter((value) => {
+                              return value !== item._id;
+                            }),
+                          ]);
+                        }
+                      }}
+                    >
+                      <View
+                        style={[
+                          tw`h-16 mb-2  mt-1 border-b-2 border-gray-200 flex flex-row`,
+                          {
+                            width: "100%",
+                          },
+                        ]}
+                      >
+                        <View>
+                          <View
+                            style={[
+                              tw``,
+                              {
+                                paddingLeft: "4%",
+                              },
+                            ]}
+                          >
+                            <Image
+                              source={{
+                                uri: `${env.DEV_SERVER_URL}api/v1/users/getImage/${item.profileImage}`,
+                              }}
+                              style={[tw` rounded-full w-14 h-14`]}
+                            />
+                          </View>
+                        </View>
+
+                        <View
+                          style={[
+                            tw`flex `,
+                            {
+                              marginLeft: "2%",
+                            },
+                          ]}
+                        >
+                          <View style={[tw``, {}]}>
+                            <Text style={[tw`font-bold text-lg`, {}]}>
+                              {item.name}
+                            </Text>
+                          </View>
+
+                          <View>
+                            <View>
+                              <Text
+                                style={[
+                                  tw`overflow-hidden italic`,
+                                  {
+                                    width: 210,
+                                  },
+                                ]}
+                                numberOfLines={1}
+                                ellipsizeMode="tail"
+                              >
+                                {item.userAbout}
+                              </Text>
+                            </View>
+                          </View>
+                        </View>
+                        <View style={[tw` m-auto`, {}]}>
+                          <Checkbox
+                            value={checkedUsers.includes(item._id)}
+                            onValueChange={() => {
+                              if (!checkedUsers.includes(item._id)) {
+                                setCheckedUsers([...checkedUsers, item._id]);
+                              } else {
+                                setCheckedUsers([
+                                  ...checkedUsers.filter((value) => {
+                                    return value !== item._id;
+                                  }),
+                                ]);
+                              }
+                            }}
+                          />
+                        </View>
+                      </View>
+                    </TouchableOpacity>
+                  </View>
+                )}
+              />
+
+              // <Text>Hello</Text>
+            )}
+
+            {checkedUsers.length > 0 ? (
+              <View
+                style={[
+                  tw`absolute bottom-0 mb-8 flex items-center justify-center`,
+                  {
+                    width: "100%",
+                  },
+                ]}
+              >
+                {/* <View style={[tw`bg-green-400 w-14 h-14 rounded-full`]}>
+            <Text>Hello World</Text>
+          </View> */}
+                <TouchableOpacity
+                  style={[
+                    tw`bg-green-400 w-14 h-14 rounded-full flex items-center justify-center`,
+                    {
+                      backgroundColor: "#128C7E",
+                    },
+                  ]}
+                  onPress={() => {
+                    setCreateGroup(true);
+                  }}
+                >
+                  <Icon name="arrowright" size={25} color="#FFFFFF" />
+                </TouchableOpacity>
+              </View>
+            ) : null}
+          </View>
+        </>
+      )}
     </>
   );
 };
